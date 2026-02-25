b5e3dd00f431d5ee160f37c41a0f56020d4e1b21
Alex Hamlin <alex@alexhamlin.co>
2026-02-24 20:02:23 -0800

    Use a single struct for progress bar stats
    Notably, rather than pre-parse all the JSON logs, this version streams
    them just as we would out of a pipe from a child process.
    
    I've effectively implemented my own version of Serde's tagged enum
    parsing, which is more powerful since I can identify different event
    types based on _multiple_ field values.
    
    I have mixed feelings on the ProgressStyle creation being an associated
    function, but it's definitely nicer outside of main().
    
    I experimented with arc-swap instead of RwLock while developing this
    changeset, but decided that simpler + stdlib was better for my needs
    than something designed for high reader contention.

--- src/bin/tiptop.rs
+++ src/bin/tiptop.rs
@@ -1,67 +1,43 @@
 use std::borrow::Cow;
 use std::env;
 use std::error::Error;
-use std::fmt::Write;
+use std::fmt::{self, Display};
 use std::fs::File;
 use std::io::BufReader;
-use std::sync::atomic::{AtomicU64, Ordering};
-use std::sync::Arc;
+use std::mem;
+use std::sync::{Arc, RwLock};
 use std::thread;
 use std::time::{Duration, Instant};
 
 use indicatif::style::ProgressTracker;
 use indicatif::{HumanBytes, ProgressBar, ProgressState, ProgressStyle};
-use serde_derive::{Deserialize, Serialize};
+use serde_derive::Deserialize;
+use serde_json::{json, Map as JsonMap, Value as JsonValue};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let transcript_path = env::args_os().nth(1).ok_or("no path provided")?;
     let transcript = File::open(transcript_path)?;
 
-    let de = serde_json::Deserializer::from_reader(BufReader::new(transcript));
-    let logs: Vec<BorgLog> = de.into_iter().collect::<Result<_, _>>()?;
-
-    println!("Parsed {len} JSON objects.", len = logs.len());
-
-    let total_nfiles = Arc::new(AtomicU64::new(0));
-    let total_orig_bytes = Arc::new(AtomicU64::new(0));
-    let total_comp_bytes = Arc::new(AtomicU64::new(0));
-    let total_ddup_bytes = Arc::new(AtomicU64::new(0));
-
-    let style = ProgressStyle::with_template(
-        "{spinner} {nfiles} N {orig} S {comp} C {ddup} D • {wide_msg}",
-    )?
-    .with_key("nfiles", AtomicCountTracker(Arc::clone(&total_nfiles)))
-    .with_key("orig", AtomicBytesTracker(Arc::clone(&total_orig_bytes)))
-    .with_key("comp", AtomicBytesTracker(Arc::clone(&total_comp_bytes)))
-    .with_key("ddup", AtomicBytesTracker(Arc::clone(&total_ddup_bytes)));
-
+    let last_progress = Arc::new(RwLock::new(ArchiveProgress::default()));
     let bar = ProgressBar::new_spinner();
-    bar.set_style(style);
+    bar.set_style(ArchiveProgress::bar_style(&last_progress));
     bar.enable_steady_tick(Duration::from_millis(100));
 
-    for log in logs {
-        let message = match log {
-            BorgLog::Unknown(_) => Cow::Borrowed("Doing stuff..."),
-            BorgLog::ProgressMessage { message, .. } => Cow::Owned(message),
-            BorgLog::ArchiveProgress {
-                nfiles,
-                original_size,
-                compressed_size,
-                deduplicated_size,
-                path,
-                finished,
-            } => {
-                if !finished {
-                    total_nfiles.store(nfiles, Ordering::Relaxed);
-                    total_orig_bytes.store(original_size, Ordering::Relaxed);
-                    total_comp_bytes.store(compressed_size, Ordering::Relaxed);
-                    total_ddup_bytes.store(deduplicated_size, Ordering::Relaxed);
-                }
+    let de = serde_json::Deserializer::from_reader(BufReader::new(transcript));
+    for raw_log in de.into_iter::<RawBorgLog>() {
+        let event = BorgEvent::from(raw_log?);
+        let bar_message = match event {
+            BorgEvent::Unknown => Cow::Borrowed("Doing some stuff..."),
+            BorgEvent::ArchiveFinished => Cow::Borrowed("Finished archiving files."),
+            BorgEvent::ProgressMessage(msg) => Cow::Owned(msg),
+            BorgEvent::ArchiveProgress(mut progress) => {
+                let path = mem::take(&mut progress.path);
+                *last_progress.write().unwrap() = progress;
                 Cow::Owned(path)
             }
         };
-        if !message.is_empty() {
-            bar.set_message(message);
+        if !bar_message.is_empty() {
+            bar.set_message(bar_message);
         }
         thread::sleep(Duration::from_millis(150));
     }
@@ -69,41 +45,100 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-#[derive(Serialize, Deserialize)]
-#[serde(tag = "type")]
-enum BorgLog {
-    #[serde(rename = "archive_progress")]
-    ArchiveProgress {
-        #[serde(default)]
-        nfiles: u64,
-        #[serde(default)]
-        original_size: u64,
-        #[serde(default)]
-        compressed_size: u64,
-        #[serde(default)]
-        deduplicated_size: u64,
-        #[serde(default)]
-        path: String,
-        #[serde(default)]
-        finished: bool,
-    },
+#[derive(Default, Deserialize)]
+#[serde(default)] // TODO: Better handling of missing "type" field.
+struct RawBorgLog {
+    #[serde(rename = "type")]
+    msg_type: String,
+    #[serde(flatten)]
+    rest: JsonMap<String, JsonValue>,
+}
 
-    #[serde(rename = "progress_message")]
-    ProgressMessage {
-        #[serde(default)]
-        msgid: String,
-        #[serde(default)]
-        message: String,
-    },
+enum BorgEvent {
+    ArchiveProgress(ArchiveProgress),
+    ArchiveFinished,
+    ProgressMessage(String),
+    Unknown,
+}
 
-    #[serde(untagged)]
-    Unknown(serde_json::Value),
+impl From<RawBorgLog> for BorgEvent {
+    fn from(raw: RawBorgLog) -> Self {
+        match raw.msg_type.as_str() {
+            "archive_progress" if raw.rest.get("finished") == Some(&json!(true)) => {
+                BorgEvent::ArchiveFinished
+            }
+            "archive_progress" => BorgEvent::ArchiveProgress(
+                serde_json::from_value(JsonValue::Object(raw.rest)).unwrap(),
+            ),
+            "progress_message" => BorgEvent::ProgressMessage(
+                raw.rest
+                    .get("message")
+                    .and_then(|v| v.as_str())
+                    .unwrap_or_default()
+                    .to_owned(),
+            ),
+            _ => BorgEvent::Unknown,
+        }
+    }
+}
+
+#[derive(Default, Deserialize)]
+#[serde(default)]
+struct ArchiveProgress {
+    nfiles: u64,
+    original_size: u64,
+    compressed_size: u64,
+    deduplicated_size: u64,
+    path: String,
+}
+
+impl ArchiveProgress {
+    fn bar_style(progress: &Arc<RwLock<ArchiveProgress>>) -> ProgressStyle {
+        ProgressStyle::with_template("{spinner} {nfiles} N {orig} S {comp} C {ddup} D • {wide_msg}")
+            .expect("hardcoded ProgressStyle template should be valid")
+            .with_key(
+                "nfiles",
+                ArchiveProgressTracker(Arc::clone(progress), ArchiveProgress::bar_nfiles),
+            )
+            .with_key(
+                "orig",
+                ArchiveProgressTracker(Arc::clone(progress), ArchiveProgress::bar_orig),
+            )
+            .with_key(
+                "comp",
+                ArchiveProgressTracker(Arc::clone(progress), ArchiveProgress::bar_comp),
+            )
+            .with_key(
+                "ddup",
+                ArchiveProgressTracker(Arc::clone(progress), ArchiveProgress::bar_ddup),
+            )
+    }
+
+    fn bar_nfiles(&self) -> u64 {
+        self.nfiles
+    }
+
+    fn bar_orig(&self) -> HumanBytes {
+        HumanBytes(self.original_size)
+    }
+
+    fn bar_comp(&self) -> HumanBytes {
+        HumanBytes(self.compressed_size)
+    }
+
+    fn bar_ddup(&self) -> HumanBytes {
+        HumanBytes(self.deduplicated_size)
+    }
 }
 
 #[derive(Clone)]
-struct AtomicCountTracker(Arc<AtomicU64>);
+struct ArchiveProgressTracker<F>(Arc<RwLock<ArchiveProgress>>, F);
 
-impl ProgressTracker for AtomicCountTracker {
+impl<F, T> ProgressTracker for ArchiveProgressTracker<F>
+where
+    F: Fn(&ArchiveProgress) -> T + Clone + Send + Sync + 'static,
+    T: Display,
+{
     fn clone_box(&self) -> Box<dyn ProgressTracker> {
         Box::new(self.clone())
     }
@@ -112,24 +147,8 @@ impl ProgressTracker for AtomicCountTracker {
 
     fn reset(&mut self, _: &ProgressState, _: Instant) {}
 
-    fn write(&self, _: &ProgressState, w: &mut dyn Write) {
-        let _ = write!(w, "{}", self.0.load(Ordering::Relaxed));
-    }
-}
-
-#[derive(Clone)]
-struct AtomicBytesTracker(Arc<AtomicU64>);
-
-impl ProgressTracker for AtomicBytesTracker {
-    fn clone_box(&self) -> Box<dyn ProgressTracker> {
-        Box::new(self.clone())
-    }
-
-    fn tick(&mut self, _: &ProgressState, _: Instant) {}
-
-    fn reset(&mut self, _: &ProgressState, _: Instant) {}
-
-    fn write(&self, _: &ProgressState, w: &mut dyn Write) {
-        let _ = write!(w, "{}", HumanBytes(self.0.load(Ordering::Relaxed)));
+    fn write(&self, _: &ProgressState, w: &mut dyn fmt::Write) {
+        let stat = self.1(&self.0.read().unwrap());
+        let _ = write!(w, "{}", stat);
     }
 }
