6057f13a92837ce20327ceb68e711a388840f00e
Alex Hamlin <alex@alexhamlin.co>
2026-03-02 19:26:43 -0800

    Experiment with the progress format for checks
    For boi's first progress bar with a known-in-advance total, I wanted to
    spend time on the formatting before getting into any JSON shenanigans.
    
    First decision: use the spinner as the line's sigil while the operation
    is running, then replace it with a final status character. This implies
    that the bar has to come later in the line.
    
    Second decision: put the bar right after the spinner, followed by the
    numeric position and length. The bar is 20 characters wide by default,
    so this ensures that multiple bars in a row all line up in the terminal.
    
    Third decision: use a bullet to separate the numeric totals from the log
    message, like before.
    
    I think this mirrors the create format reasonably well, and I think
    it'll scale well to other operations. Maybe I'll even be able to use
    this template for every operation other than create.

--- src/check.rs
+++ src/check.rs
@@ -1,3 +1,7 @@
+use std::time::Duration;
+
+use indicatif::{ProgressBar, ProgressStyle};
+
 use crate::child::{self, Child};
 
 #[derive(clap::Args)]
@@ -5,12 +9,46 @@ pub struct Args {
     /// Only perform repository checks (chunk CRCs)
     #[arg(long)]
     repository_only: bool,
+
+    /// Actually do the check instead of rendering a fake bar (TEMPORARY)
+    #[arg(long)]
+    actually_check: bool,
 }
 
 pub async fn main(args: Args) -> child::Result<()> {
+    render().await;
+    if !args.actually_check {
+        return Ok(());
+    }
+
     let mut cmdline = vec!["borg", "check", "-v", "--progress"];
     if args.repository_only {
         cmdline.push("--repository-only");
     }
     Child::from_cmdline(&cmdline).complete().await
+}
+
+async fn render() {
+    let style = ProgressStyle::with_template("[boi] {spinner} {bar} {pos}/{len} • {wide_msg}")
+        .expect("hardcoded ProgressStyle template should be valid");
+
+    let bar = ProgressBar::no_length();
+    bar.set_style(style.clone());
+    bar.enable_steady_tick(Duration::from_millis(100));
+    bar.set_message("Waiting for Borg to start");
+
+    bar.tick();
+    tokio::time::sleep(Duration::from_secs(2)).await;
+
+    bar.set_message("Analyzing the stuff");
+    bar.set_length(41);
+    for _ in 0..41 {
+        tokio::time::sleep(Duration::from_millis(195)).await;
+        bar.inc(1);
+    }
+
+    bar.finish_and_clear();
+    speak!("✓", "Checked all the stuff");
+
+    // NOTE: there are multiple phases with different lengths, delineated by the msgid field.
 }
