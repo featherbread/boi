49ba0b686f1aee17316ae69363d0741ab2484973
Alex Hamlin <alex@alexhamlin.co>
2026-08-19 20:12:58 -0700

    Workshop the comment on boi::json::JsonStream

--- src/json.rs
+++ src/json.rs
@@ -10,20 +10,23 @@ use tokio_util::io::SyncIoBridge;
 
 /// Iterates over JSON objects streamed from an [`AsyncRead`].
 ///
-/// This synchronously parses the JSON events in another thread via [`SyncIoBridge`].
-/// Why can't we use a pure async solution?
+/// Each stream spawns an independent thread (outside of any Tokio thread pool) to synchronously
+/// parse JSON events via [`SyncIoBridge`], which terminates at the end of the stream or after the
+/// stream is dropped.
 ///
-/// 1. We can't buffer all the raw JSON into memory (as the `SyncIoBridge` docs suggest),
-///    since it may be used to update live state like progress bars.
+/// # Why [`SyncIoBridge`]?
 ///
-/// 2. We can't iterate over lines, since not all JSON streams are guaranteed to use the JSON Lines
-///    format. In particular, Borg is known to pretty-print non-log JSON output.
+/// A purely asynchronous implementation would either:
 ///
-/// Strictly speaking, 2. is an artifact of boi's own choice to combine standard output and error
-/// into a single stream; Borg itself uses JSON lines on stderr and pretty-prints by default only
-/// on stdout. Newer Borg versions also have options to disable this pretty-printing. However,
-/// making boi more liberal makes it more compatible with old Borg versions or alternate harnesses
-/// that emulate Borg's combined output.
+/// 1. Require full buffering, inhibiting the use of these streams to update live state like
+///    terminal progress bars
+///
+/// 2. Impose additional restrictions on the input format, like requiring JSON Lines with no
+///    possibility for pretty-printing
+///
+/// For boi in particular, lifting the second restriction simplifies compatibility with Borg
+/// versions that don't implement `$BORG_JSON_INDENT` (v1.4.5+) without requiring the test harness
+/// to emulate Borg's splitting of pretty and non-pretty JSON between stdout and stderr.
 pub struct JsonStream<T>(mpsc::UnboundedReceiver<serde_json::Result<T>>)
 where
     T: DeserializeOwned + Send + 'static;
